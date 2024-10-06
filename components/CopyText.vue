5ca23d0691ed42a92c724f4dd746a35523b94d2f
nicholas <nicholas.us.03@gmail.com>
2024-10-06 13:21:02 +0300

    update link

---
+++ components/CopyText.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const currentYear = ref(new Date().getFullYear())
+</script>
+
+<template>
+  <div class="copy-text">© Santos Ltd {{ currentYear }} ACN 007550923</div>
+</template>
+
+<style scoped>
+
+</style>
