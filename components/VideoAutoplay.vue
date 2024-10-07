4a557c26e81f0e888c01166ddf5bf570a9fdd6f8
nicholas <nicholas.us.03@gmail.com>
2024-10-07 21:39:14 +0300

    update santos

---
+++ components/VideoAutoplay.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <video class="w-100" loop="loop" autoplay="autoplay" preload="auto" muted playsinline="">
+    <source src="/assets/video/video_1.mp4" type="video/mp4">
+  </video>
+</template>
+
+<style scoped>
+.w-100 {
+  width: 100%;
+}
+</style>
