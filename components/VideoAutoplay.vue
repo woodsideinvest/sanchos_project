ff50e690191bc1614087af0bf8c79a78b2000e07
nicholas <nicholas.us.03@gmail.com>
2024-10-07 22:29:25 +0300

    update text top

--- components/VideoAutoplay.vue
+++ components/VideoAutoplay.vue
@@ -1,15 +1,26 @@
 <script setup>
+import {ref, onMounted} from 'vue';
 
+const isVideoLoaded = ref(false);
+
+onMounted(() => {
+  const videoElement = document.querySelector('video');
+
+  videoElement.addEventListener('loadeddata', () => {
+    isVideoLoaded.value = true;
+  });
+});
 </script>
 
 <template>
-  <video class="w-100" loop="loop" autoplay="autoplay" preload="auto" muted playsinline="">
-    <source src="/assets/video/video_1.mp4" type="video/mp4">
-  </video>
+    <video v-if="isVideoLoaded"  class="w-100" loop autoplay preload="auto" muted playsinline>
+      <source src="/assets/video/video_1.mp4" type="video/mp4">
+    </video>
 </template>
 
 <style scoped>
 .w-100 {
   width: 100%;
+  height: 100%;
 }
 </style>
