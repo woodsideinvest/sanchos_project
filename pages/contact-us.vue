896be1f598e7e6d9df68057f917a978579d7c1a4
nicholas <nicholas.us.03@gmail.com>
2024-10-08 16:56:53 +0300

    update santos

---
+++ pages/contact-us.vue
@@ -0,0 +1,457 @@
+<script setup>
+definePageMeta({
+  layout: 'upstream'
+})
+
+import {onMounted, onBeforeMount} from 'vue';
+
+onBeforeMount(() => {
+  const element = document.body;
+  if (element) {
+    element.className = '';
+    element.classList.add('page', 'page-id-10965', 'dark', 'upstream-gas-liquids', 'has-loaded', 'pace-done');
+  }
+});
+
+</script>
+
+<template>
+  <div class="banner-cover banner--up">
+    <div class="banner-overlay">
+      <div class="breadcrumb">
+        <div class="container">
+          <div class="breadcrub-wrapper">
+            <!-- Breadcrumb NavXT 7.2.0 -->
+            <span property="itemListElement" typeof="ListItem"><a href="/" property="item" typeof="WebPage" title="Go to Contact us." class=""><span property="name">
+              <i class="fas fa-home-lg"></i></span></a><meta property="position" content="1"></span><i class="fas fa-caret-right"></i><span class="">Contact us</span>                          </div>
+        </div>
+      </div>
+
+      <div class="container banner-content">
+        <div class="row">
+          <div class="col-12">
+
+
+            <div class="banner-cta ">
+              <h1>
+                Contact us
+
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="anchor-indicator d-none d-xs-block">
+        <a href="#" data-target-section="1"><img src="/assets/images/mouse-clicker_b28c10c8.png" alt="Scroll down"></a>
+      </div>
+    </div>
+  </div>
+  <main class="main" id="main">
+    <div class="container">
+
+      <section class="one-column-row" data-sectionid="1">
+        <div class="one-col-inner-wrp ">
+          <p><a class="button telegram" style="background: #0693e3; font-size: 2rem;" href="https://t.me/santos_ltd_manager" target="_blank"
+                rel="nofollow noopener"><strong>Contact us</strong></a></p>
+        </div>
+      </section>
+
+
+      <section class="spacer empty-spacer" data-sectionid="2">
+      </section>
+
+
+      <section class="three-column-row" data-sectionid="3">
+        <div class="row">
+          <div class="col-lg-4">
+            <h2>Talk to us</h2>
+          </div>
+          <div class="col-lg-4">
+            <h3><strong>General enquiries</strong></h3>
+            <h6><strong>9am to 5pm, Monday to Friday</strong><br>
+              <a href="#">+61 8 8116 5000</a></h6>
+          </div>
+          <div class="col-lg-4">
+            <h3><strong>Shareholder enquiries</strong></h3>
+            <h6><strong>Computershare Investor Services Pty Ltd</strong></h6>
+            <p>1300 096 259 (Australia)<br>
+              +61 3 9415 4397 (International)</p>
+          </div>
+        </div>
+      </section>
+
+
+      <section class="spacer empty-spacer" data-sectionid="4">
+      </section>
+
+
+      <section class="heading no-padding" data-sectionid="5">
+        <h2>Our locations</h2>
+      </section>
+
+
+      <section class="accordions" data-sectionid="6">
+        <!-- panel-group accordion -->
+        <div id="accordion_6" class="accordion-list" role="tablist" aria-multiselectable="true">
+
+          <!-- panel -->
+          <div class="card">
+            <div class="card-heading" role="tab" id="heading_accordion_6_tab_1">
+              <h5 class="mb-0">
+                <a role="button" data-toggle="collapse" data-target="#accordion_6_tab_1" href="#accordion_6_tab_1" aria-expanded="false" aria-controls="accordion_6_tab_1" class="accordion-opener collapsed">
+                  South Australia            </a>
+              </h5>
+            </div>
+            <div id="accordion_6_tab_1" class="collapse" role="tabpanel" aria-labelledby="heading_accordion_6_tab_1" data-parent="#accordion_6">
+              <div class="card-block">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h3>Adelaide</h3>
+                    <p><strong>Registered and Head Office, Share Register Santos Ltd</strong><br>
+                      Ground Floor Santos Centre<br>
+                      <a target="_blank" rel="noopener">60 Flinders Street</a><br>
+                      Adelaide<br>
+                      South Australia 5000<br>
+                      AUSTRALIA</p>
+                    <p><strong>Phone:</strong>&nbsp;<a href="">+61 8 8116 5000</a><br>
+                      <strong>Fax:</strong>&nbsp;+61 8 8116 5050</p>
+                    <p><strong>Post:<br>
+                    </strong>GPO Box 2455<br>
+                      Adelaide<br>
+                      South Australia 5001<br>
+                      AUSTRALIA</p>
+                  </div>
+                  <div class="col-md-6">
+                    <h3>Port Bonython</h3>
+                    <p><strong>Phone:</strong> <a href="">+61 8 8649 0100</a><br>
+                      <strong>Fax:</strong> +61 8 8649 0200</p>
+                    <p><strong>Post:<br>
+                    </strong>PO Box 344<br>
+                      Whyalla<br>
+                      South Australia 5600<br>
+                      AUSTRALIA</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <!-- panel -->
+          <div class="card">
+            <div class="card-heading" role="tab" id="heading_accordion_6_tab_2">
+              <h5 class="mb-0">
+                <a role="button" data-toggle="collapse" data-target="#accordion_6_tab_2" href="#accordion_6_tab_2" aria-expanded="false" aria-controls="accordion_6_tab_2" class="accordion-opener collapsed">
+                  Western Australia            </a>
+              </h5>
+            </div>
+            <div id="accordion_6_tab_2" class="collapse" role="tabpanel" aria-labelledby="heading_accordion_6_tab_2" data-parent="#accordion_6">
+              <div class="card-block">
+                <div class="row">
+                  <div class="col-md-12">
+                    <h3>Perth</h3>
+                    <p><a target="_blank" rel="noopener">Level 7, 100 St Georges Terrace</a><br>
+                      Perth<br>
+                      Western Australia 6000<br>
+                      AUSTRALIA</p>
+                    <p><strong>Post:</strong><br>
+                      PO Box 5624<br>
+                      Perth<br>
+                      Western Australia 6000<br>
+                      AUSTRALIA</p>
+                    <p><strong>Phone: </strong><a href="#">+61 8 6218 7100</a><br>
+                      <strong>Fax: </strong>+61 8 6218 7200</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <!-- panel -->
+          <div class="card">
+            <div class="card-heading" role="tab" id="heading_accordion_6_tab_3">
+              <h5 class="mb-0">
+                <a role="button" data-toggle="collapse" data-target="#accordion_6_tab_3" href="#accordion_6_tab_3" aria-expanded="false" aria-controls="accordion_6_tab_3" class="accordion-opener collapsed">
+                  New South Wales            </a>
+              </h5>
+            </div>
+            <div id="accordion_6_tab_3" class="collapse" role="tabpanel" aria-labelledby="heading_accordion_6_tab_3" data-parent="#accordion_6">
+              <div class="card-block">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h3>Narrabri</h3>
+                    <p><a target="_blank" rel="noopener">125 Maitland Street</a><br>
+                      Narrabri<br>
+                      New South Wales 2390<br>
+                      AUSTRALIA</p>
+                    <p><strong>Phone: </strong><a href="">+61 2 6792 9035</a></p>
+                  </div>
+                  <div class="col-md-6">
+                    <h3>Sydney</h3>
+                    <p><a target="_blank" rel="noopener">1 Bligh Street</a><br>
+                      Sydney<br>
+                      New South Wales 2000<br>
+                      AUSTRALIA</p>
+                    <p><strong>Post:</strong><br>
+                      GPO Box 2442<br>
+                      Sydney<br>
+                      New South Wales 2001<br>
+                      AUSTRALIA</p>
+                    <p><strong>Phone:</strong>&nbsp;+61 8 8116 5000<br>
+                      <strong>Fax:</strong>&nbsp;+61 8 8116 5050</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <!-- panel -->
+          <div class="card">
+            <div class="card-heading" role="tab" id="heading_accordion_6_tab_4">
+              <h5 class="mb-0">
+                <a role="button" data-toggle="collapse" data-target="#accordion_6_tab_4" href="#accordion_6_tab_4" aria-expanded="false" aria-controls="accordion_6_tab_4" class="accordion-opener collapsed">
+                  Queensland            </a>
+              </h5>
+            </div>
+            <div id="accordion_6_tab_4" class="collapse" role="tabpanel" aria-labelledby="heading_accordion_6_tab_4" data-parent="#accordion_6">
+              <div class="card-block">
+                <div class="row">
+                  <div class="col-md-4">
+                    <h3>Brisbane</h3>
+                    <p>Level 22, Santos Place<br>
+                      <a target="_blank" rel="noopener">32 Turbot Street</a><br>
+                      Brisbane<br>
+                      Queensland 4000<br>
+                      AUSTRALIA</p>
+                    <p><strong>Phone:</strong> <a href="">+61 7 3838 3000</a><br>
+                      <strong>Fax:</strong> +61 7 3838 3350</p>
+                    <p><strong>Post:<br>
+                    </strong>Reply Paid<br>
+                      GLNG Community Engagement<br>
+                      GPO Box 1010<br>
+                      Brisbane<br>
+                      Queensland 4001<br>
+                      AUSTRALIA</p>
+                  </div>
+                  <div class="col-md-4">
+                    <h3>Gladstone</h3>
+                    <p><strong>Phone:</strong> <a href="">1800 761 113</a><br>
+                      <strong>Email:</strong> <a >glng.community@glng.com</a></p>
+                    <p><strong>Post:<br>
+                    </strong>Santos GLNG<br>
+                      Reply Paid<br>
+                      GLNG Community Engagement<br>
+                      PO Box 341<br>
+                      Gladstone<br>
+                      Queensland 4680<br>
+                      AUSTRALIA</p>
+                  </div>
+                  <div class="col-md-4">
+                    <h3>Roma</h3>
+                    <p><a >94 McDowall Street</a><br>
+                      Roma, Queensland 4455<br>
+                      AUSTRALIA</p>
+                    <p><strong>Phone:</strong> <a href="">1800 761 113</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <!-- panel -->
+          <div class="card">
+            <div class="card-heading" role="tab" id="heading_accordion_6_tab_5">
+              <h5 class="mb-0">
+                <a role="button" data-toggle="collapse" data-target="#accordion_6_tab_5" href="#accordion_6_tab_5" aria-expanded="false" aria-controls="accordion_6_tab_5" class="accordion-opener collapsed">
+                  Northern Territory            </a>
+              </h5>
+            </div>
+            <div id="accordion_6_tab_5" class="collapse" role="tabpanel" aria-labelledby="heading_accordion_6_tab_5" data-parent="#accordion_6">
+              <div class="card-block">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h3>Darwin office</h3>
+                    <p>T14/15<br>
+                      41 The Mall<br>
+                      Darwin<br>
+                      Northern Territory 0800<br>
+                      AUSTRALIA</p>
+                    <p><strong>Post:</strong><br>
+                      PO Box 2266<br>
+                      Darwin GPO<br>
+                      Northern Territory 0800<br>
+                      AUSTRALIA</p>
+                    <p><strong>Phone:</strong> 08 8919 1900</p>
+                  </div>
+                  <div class="col-md-6">
+                    <h3>Darwin LNG facility</h3>
+                    <p>24-hour community telephone line: <a href="">1800 267 600</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <!-- panel -->
+          <div class="card">
+            <div class="card-heading" role="tab" id="heading_accordion_6_tab_6">
+              <h5 class="mb-0">
+                <a role="button" data-toggle="collapse" data-target="#accordion_6_tab_6" href="#accordion_6_tab_6" aria-expanded="false" aria-controls="accordion_6_tab_6" class="accordion-opener collapsed">
+                  Asia Pacific            </a>
+              </h5>
+            </div>
+            <div id="accordion_6_tab_6" class="collapse" role="tabpanel" aria-labelledby="heading_accordion_6_tab_6" data-parent="#accordion_6">
+              <div class="card-block">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h3>Papua New Guinea</h3>
+                    <p>Oil Search (PNG) Limited<br>
+                      Ground Floor<br>
+                      <a >Harbourside East Building</a><br>
+                      Stanley Esplanade<br>
+                      Port Moresby<br>
+                      PAPUA NEW GUINEA</p>
+                    <p><strong>Post:</strong><br>
+                      PO Box 842<br>
+                      Port Moresby<br>
+                      NCD 121<br>
+                      PAPUA NEW GUINEA</p>
+                    <p><strong>Phone:</strong> +675 322 5599<br>
+                      <strong>Fax:</strong> +675 322 5566</p>
+                  </div>
+                  <div class="col-md-6">
+                    <h3>Timor-Leste</h3>
+                    <p>Unit 401, Level 4 – CBD3<br>
+                      Dili Central Comoro/Timor Plaza Building<br>
+                      Dili<br>
+                      TIMOR-LESTE</p>
+                    <p><strong>Post:</strong><br>
+                      PO Box 335<br>
+                      Dili<br>
+                      TIMOR-LESTE</p>
+                    <p><strong>Phone:</strong> +670 331 1275</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <!-- panel -->
+          <div class="card">
+            <div class="card-heading" role="tab" id="heading_accordion_6_tab_7">
+              <h5 class="mb-0">
+                <a role="button" data-toggle="collapse" data-target="#accordion_6_tab_7" href="#accordion_6_tab_7" aria-expanded="false" aria-controls="accordion_6_tab_7" class="accordion-opener collapsed">
+                  North America            </a>
+              </h5>
+            </div>
+            <div id="accordion_6_tab_7" class="collapse" role="tabpanel" aria-labelledby="heading_accordion_6_tab_7" data-parent="#accordion_6">
+              <div class="card-block">
+                <div class="row">
+                  <div class="col-md-12">
+                    <h3>Alaska</h3>
+                    <p>601 W 5th Avenue<br>
+                      Anchorage<br>
+                      Alaska 99501<br>
+                      UNITED STATES</p>
+                    <p><strong>Post:</strong><br>
+                      PO Box 240927<br>
+                      Anchorage<br>
+                      Alaska 99524-0927<br>
+                      UNITED STATES</p>
+                    <p><strong>Phone:</strong> +907 375 4600<br>
+                      <strong>Fax:</strong> +907 375 4630</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.banner-cover {
+  background-image: url(/assets/images/sncn20.jpg) !important;
+}
+
+@media (min-width: 992px) {
+  .banner-cover {
+    background-image: url(/assets/images/sncn20.jpg) !important;
+  }
+}
+.responsive-table__instruction {
+  display: none !important;
+}
+
+
+select {
+  //width: 50%;
+  height: 60px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: #fff;
+  color: #333;
+}
+
+
+/* Стили для элемента <input> */
+input[type="number"] {
+  width: 50%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: #fff;
+  color: #333;
+}
+
+#result_gas #income_gas {
+  color: black;
+}
+
+#income_energy {
+  color: black;
+}
+
+.button.telegram {
+  background-color: #d71538;
+  color: #fff;
+  padding: 10px 20px;
+  text-decoration: none;
+  border-radius: 4px;
+  font-size: 16px;
+  transition: background-color 0.3s ease;
+}
+
+.button.telegram:hover {
+  background-color: #ff1a4d;
+}
+
+/* Стили для элемента select и input для мобилки */
+@media (max-width: 767px) {
+  select {
+    width: 100%;
+  }
+
+  input[type="number"] {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    background-color: #fff;
+    color: #333;
+  }
+}
+</style>
